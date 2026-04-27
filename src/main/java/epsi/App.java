7e32165fc986cc907fefe54a70cacc614ed5b4d0
Rayen <rayenmabrouk9@gmail.com>
2026-04-27 11:25:38 +0100

    Ajout structure Maven avec tests

---
+++ src/main/java/epsi/App.java
@@ -0,0 +1,10 @@
+package epsi;
+
+public class App {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0].trim());
+        int b = Integer.parseInt(args[1].trim());
+        System.out.println("Somme = " + (a + b));
+        System.out.println("Produit = " + (a * b));
+    }
+}
---
+++ src/test/java/epsi/AppTest.java
@@ -0,0 +1,12 @@
+package epsi;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        App app = new App();
+        assertNotNull(app);
+    }
+}
