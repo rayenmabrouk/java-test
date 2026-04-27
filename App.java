595d2fc5097df588666c374d598340fef0c82662
Rayen <rayenmabrouk9@gmail.com>
2026-04-27 11:35:25 +0100

    Ajout avertissements pour Warnings Next Generation

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
+import java.util.Date;
+
 public class App {
+    static int unusedField = 0;
+
     public static void main(String[] args) {
+        Date date = new Date(2024, 1, 1);
         int a = Integer.parseInt(args[0].trim());
         int b = Integer.parseInt(args[1].trim());
         System.out.println("Somme = " + (a + b));
